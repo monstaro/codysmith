96a703fd57df6a682365879730010ffaa3300eef
monstaro <codysmith2814@gmail.com>
2021-02-26 16:48:59 -0700

    Update homepage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import splash from "./splash.gif";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="name">cody smith</h1>
+        <img src={splash} className="App-logo" alt="logo" />
+        <h2 className="name small">web developer / musician</h2>
+        <h3 className="name small">portfolio coming soon</h3>
       </header>
     </div>
   );
